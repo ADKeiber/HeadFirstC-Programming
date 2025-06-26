340cbbdd4af2ee9cf4f777eee3db341dbc623ab0
Austin Keiber <adkeiber@gmail.com>
2025-06-25 21:03:40 -0400

    categorize safe file open update

--- ch3/categorize.c
+++ ch3/categorize.c
@@ -13,11 +13,27 @@ int main(int argc, char *argv[]) {
         fprintf(stderr, "You need to give 5 arguemnts\n");
         return 1;
     }
+    FILE *in;
+    FILE *file1;
+    FILE *file2;
+    FILE *file3;
 
-    FILE *in = fopen("spooky.csv", "r");
-    FILE *file1 = fopen(argv[2], "w");
-    FILE *file2 = fopen(argv[4], "w");
-    FILE *file3 = fopen(argv[5], "w");
+    if(!(in = fopen("spooky.csv", "r"))) {
+        fprintf(stderr, "Cant open the spooky file.\n");
+        return 1;
+    }  
+    if(!(file1 = fopen(argv[2], "w"))) {
+        fprintf(stderr, "Cant open the first file.\n");
+        return 1;
+    }
+    if(!(file2 = fopen(argv[4], "w"))) {
+        fprintf(stderr, "Cant open the second file.\n");
+        return 1;
+    }
+    if(!(file3 = fopen(argv[5], "w"))) {
+        fprintf(stderr, "Cant open the third file.\n");
+        return 1;
+    }
     
     while(fscanf(in, "%79[^\n]\n", line) == 1) {
 
@@ -28,6 +44,7 @@ int main(int argc, char *argv[]) {
         else 
             fprintf(file3, "%s\n", line);
     }
+
     fclose(file1);
     fclose(file2);
     fclose(file3);
