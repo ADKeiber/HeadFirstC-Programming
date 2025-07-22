50c0069448ee9ddfb77bacfd928f0ec3e4f8da51
Austin Keiber <adkeiber@gmail.com>
2025-07-21 21:24:21 -0400

    dear john

---
+++ ch7/dear_john.c
@@ -0,0 +1,47 @@
+/*
+* Dear john automater
+* Taken from 'Head First C'
+*/
+#include <stdio.h>
+
+enum response_type {DUMP, SECOND_CHANGE, MARRIAGE};
+typedef struct {
+    char *name;
+    enum response_type type;
+} response;
+
+void dump(response r) {
+    printf("Dear %s,\n", r.name);
+    puts("Unfortunately your last date contacted us to");
+    puts("say that they will not be seeing you again");
+}
+
+void second_chance(response r) {
+    printf("Dear %s,\n", r.name);
+    puts("Good news: your last date has asked us to");
+    puts("arrange another meeting. Please call ASAP.");
+}
+
+void marriage(response r) {
+    printf("Dear %s,\n", r.name);
+    puts("Congratulations! Your last date has contacted");
+    puts("us with a proposal of marriage.");
+}
+
+int main() {
+    response r[] = {{"Mike", DUMP}, {"Luis", SECOND_CHANGE}, {"Matt", SECOND_CHANGE}, {"Mike", MARRIAGE}};
+    int i;
+    for(i=0; i < 4; i++) {
+        switch(r[i].type) {
+            case DUMP:
+                dump(r[i]);
+                break;
+            case SECOND_CHANGE:
+                second_chance(r[i]);
+                break;
+            default:
+                marriage(r[i]);
+        }
+    }
+    return 0;
+}
