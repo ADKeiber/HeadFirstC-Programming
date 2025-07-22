357afdb8355fa003c55b8ecde1950c91eac57358
Austin Keiber <adkeiber@gmail.com>
2025-07-21 21:36:16 -0400

    update dear john to use function pointers

--- ch7/dear_john.c
+++ ch7/dear_john.c
@@ -28,20 +28,13 @@ void marriage(response r) {
     puts("us with a proposal of marriage.");
 }
 
+void (*replies[]) (response) = {dump, second_chance, marriage};
+
 int main() {
     response r[] = {{"Mike", DUMP}, {"Luis", SECOND_CHANGE}, {"Matt", SECOND_CHANGE}, {"Mike", MARRIAGE}};
     int i;
     for(i=0; i < 4; i++) {
-        switch(r[i].type) {
-            case DUMP:
-                dump(r[i]);
-                break;
-            case SECOND_CHANGE:
-                second_chance(r[i]);
-                break;
-            default:
-                marriage(r[i]);
-        }
+        (replies[r[i].type])(r[i]);
     }
     return 0;
 }
