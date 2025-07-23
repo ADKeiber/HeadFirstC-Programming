9686c90bf0d6836ce3286acdabdddc77203ddc4b
Austin Keiber <adkeiber@gmail.com>
2025-07-22 20:22:51 -0400

    drink

--- ch7/dear_john.c
+++ ch7/dear_john.c
@@ -34,7 +34,7 @@ int main() {
     response r[] = {{"Mike", DUMP}, {"Luis", SECOND_CHANGE}, {"Matt", SECOND_CHANGE}, {"Mike", MARRIAGE}};
     int i;
     for(i=0; i < 4; i++) {
-        (replies[r[i].type])(r[i]);
+        replies[r[i].type](r[i]);
     }
     return 0;
 }
