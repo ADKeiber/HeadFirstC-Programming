3fdbb277d09e475118ac842960de7cb329078d62
Austin Keiber <adkeiber@gmail.com>
2025-07-24 19:23:13 -0400

    elliptical stuffA

---
+++ ch8/calorie_counter/elliptical.c
@@ -0,0 +1,11 @@
+/**
+ * Displays on the elliptical
+ * Taken from 'Head first C'
+ */
+#include <stdio.h>
+#include "hfcal.h"
+
+int main() {
+    display_calories(115.2, 11/3, 0.79);
+    return 0;
+}
